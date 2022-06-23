452321159b9b2e89249ffbcf0924e34d3518c07d
alokkumar-dev <alokshakya2601@gmail.com>
2022-06-23 17:46:57 +0530

    add search icons

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,14 +1,20 @@
 import "./HomePage.css"
+import locationLogo from "../../Icons/location.png"
+import search from "../../Icons/search.png"
 export const HomePage = () => {
   return (
     <>
       <div className="homeContainer">
         <div className="searchBar">
-          <div className="logoDiv"></div>
+          <div className="logoDiv">
+            <img src={locationLogo} alt="location" />
+          </div>
           <div className="inputDiv">
             <input type="text" className="inputBox" placeholder="enter your city"/>
           </div>
-          <div className="searchIconDiv"></div>
+          <div className="searchIconDiv">
+            <img src={search} alt="search icon" />
+          </div>
         </div>
       </div>
     </>
