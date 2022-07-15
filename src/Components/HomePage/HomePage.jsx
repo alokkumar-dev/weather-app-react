3d4cc2bc5d29aa381921afa9eeba72c21eb4f962
alokkumar-dev <alokshakya2601@gmail.com>
2022-07-15 18:40:54 +0530

    add some functionality and css

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import locationLogo from "../../Icons/location.png";
 import searchIcon from "../../Icons/search.png";
 import { useEffect, useState } from "react";
 import { Graph } from "./TempGraph/TempGraph";
-import sunRiseSet from "../../Icons/sunSet.png"
+import sunRiseSet from "../../Icons/sunSet.png";
 export const HomePage = () => {
   const [searchWeather, setSetWeather] = useState("pune");
   const [weather, setWeather] = useState([]);
@@ -13,12 +13,11 @@ export const HomePage = () => {
   }, [searchWeather]);
   const handleChange = (e) => {
     setSetWeather(e.target.value);
-
   };
 
   const getData = async () => {
     // let city = "pune"
-   
+
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=000ea10fae727b5e0d08edbb2b5f07c0`;
     try {
       let res = await fetch(url);
@@ -62,37 +61,48 @@ export const HomePage = () => {
           </div>
         </div>
         <div className="weather-sevenDays">
-          { 
-          weather?
-          weather.map((el, index) => (
-            <div key={index}>
-              <div className="tempDivs">
-                <h5>{Math.floor(el.temp.max - 273)}°C</h5>
-                <h5>{Math.floor(el.temp.max - 273)}°C</h5>
-              </div>
+          {weather ? (
+            weather.map((el, index) => (
+              <div key={index}>
+                <div className="tempDivs">
+                  <h5>{Math.floor(el.temp.max - 273)}°C</h5>
+                  <h5>{Math.floor(el.temp.max - 273)}°C</h5>
+                </div>
 
-              <img
-                src={`https://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`}
-                alt="temprecutre logo"
+                <img
+                  src={`https://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`}
+                  alt="temprecutre logo"
                 />
                 <p>{el.weather[0].description}</p>
-            </div>
-          )) : <h1>Loading...</h1>
-        }
+              </div>
+            ))
+          ) : (
+            <h1>Loading...</h1>
+          )}
         </div>
         <div className="temp_graph">
           <Graph />
-        <div className="pressur_humidity">
-          <div>
-            <h3>Pressure</h3>
-            <p>1001 hpa</p>
+          <div className="pressur_humidity">
+            <div>
+              <h3>Pressure</h3>
+              <p>1001 hpa</p>
+            </div>
+            <div>
+              <h3>Humidity</h3>
+              <p>1001 hpa</p>
+            </div>
           </div>
-          <div>
-            <h3>Humidity</h3>
-            <p>1001 hpa</p>
+          <div className="pressur_humidity">
+            <div>
+              <h3>Sunrise</h3>
+              <p>5:0 AM</p>
+            </div>
+            <div>
+              <h3>Sunset</h3>
+              <p>6:45 PM</p>
+            </div>
           </div>
-        </div>
-          <img src={sunRiseSet} alt="sunRiseSet" />
+          <img src={sunRiseSet} width="100%" alt="sunRiseSet" />
         </div>
       </div>
     </>
