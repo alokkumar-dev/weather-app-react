6332e20a01ebda811b951154014a3a39e54b5e62
alokkumar-dev <alokshakya2601@gmail.com>
2022-07-16 11:45:18 +0530

    modidy functionality and css

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,20 +4,30 @@ import searchIcon from "../../Icons/search.png";
 import { useEffect, useState } from "react";
 import { Graph } from "./TempGraph/TempGraph";
 import sunRiseSet from "../../Icons/sunSet.png";
-export const HomePage = () => {
-  const [searchWeather, setSetWeather] = useState("pune");
-  const [weather, setWeather] = useState([]);
+import Chart from "react-apexcharts";
+import axios from "axios";
 
-  useEffect(() => {
-    getData();
-  }, [searchWeather]);
-  const handleChange = (e) => {
-    setSetWeather(e.target.value);
+export const HomePage = () => {
+  const [searchWeather, setSearchWeather] = useState("");
+  const [weather, setWeather] = useState([]);
+  const [dailyWeather, setDailyWeather] = useState([]);
+
+  const dailyData = (e) => {
+    // console.log(e)
+    let arr = e.temp;
+    console.log(e);
+    //  setIcon(e.weather[0].icon)
+    //   // console.log(e,"onclick")
+    setDailyWeather(arr);
   };
+  useEffect(() => {
+    axios.get("https://ipinfo.io/json?token=52ed0181817dc8").then((res) => {
+      // console.log(res.data.city)
+      setSearchWeather(res.data.city);
+    });
+  }, []);
 
   const getData = async () => {
-    // let city = "pune"
-
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=000ea10fae727b5e0d08edbb2b5f07c0`;
     try {
       let res = await fetch(url);
@@ -31,7 +41,7 @@ export const HomePage = () => {
   };
 
   const getDatafor7days = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=000ea10fae727b5e0d08edbb2b5f07c0`;
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=000ea10fae727b5e0d08edbb2b5f07c0`;
     try {
       let res = await fetch(url);
       let data = await res.json();
@@ -40,6 +50,13 @@ export const HomePage = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+  useEffect(() => {
+    getData();
+    // dailyData("Agra");
+  }, [searchWeather]);
+  const handleChange = (e) => {
+    setSearchWeather(e.target.value);
   };
   return (
     <>
@@ -52,6 +69,7 @@ export const HomePage = () => {
             <input
               type="text"
               onChange={handleChange}
+              value={searchWeather}
               className="inputBox"
               placeholder="enter your city"
             />
@@ -63,10 +81,10 @@ export const HomePage = () => {
         <div className="weather-sevenDays">
           {weather ? (
             weather.map((el, index) => (
-              <div key={index}>
+              <div key={index} onClick={() => dailyData(el)}>
                 <div className="tempDivs">
-                  <h5>{Math.floor(el.temp.max - 273)}°C</h5>
-                  <h5>{Math.floor(el.temp.max - 273)}°C</h5>
+                  <h5>{Math.floor(el.temp.min)}°C</h5>
+                  <h5>{Math.floor(el.temp.max)}°C</h5>
                 </div>
 
                 <img
@@ -81,7 +99,77 @@ export const HomePage = () => {
           )}
         </div>
         <div className="temp_graph">
-          <Graph />
+          <div className="Graph">
+            {dailyWeather.day?
+            <Chart
+              type="area"
+              series={[
+                {
+                  name: "Temperature",
+                  data: [
+                    dailyWeather.morn,
+                    dailyWeather.max,
+                    dailyWeather.day,
+                    dailyWeather.min,
+                  ],
+                },
+              ]}
+              options={{
+                dataLabels: {
+                  formatter: (val) => {
+                    // return `${val}℃`;
+                  },
+                },
+                yaxis: {
+                  labels: {
+                    formatter: (val) => {
+                      return `${Math.floor(val)}℃`;
+                    },
+                  },
+                },
+                xaxis: {
+                  categories: ["6:00am", "12:00pm", "6:00pm", "12:00am"],
+                },
+              }}
+            /> : <Chart
+            type="area"
+            series={[
+              {
+                name: "Temperature",
+                data: [
+                  // dailyWeather.morn,
+                  // dailyWeather.max,
+                  // dailyWeather.day,
+                  // dailyWeather.min,
+                  20,
+                  35,
+                  25,
+                  18
+
+                ],
+              },
+            ]}
+            options={{
+              dataLabels: {
+                formatter: (val) => {
+                  // return `${val}℃`;
+                },
+              },
+              yaxis: {
+                labels: {
+                  formatter: (val) => {
+                    return `${Math.floor(val)}℃`;
+                  },
+                },
+              },
+              xaxis: {
+                categories: ["6:00am", "12:00pm", "6:00pm", "12:00am"],
+              },
+            }}
+          />
+
+}
+          </div>
           <div className="pressur_humidity">
             <div>
               <h3>Pressure</h3>
@@ -89,7 +177,7 @@ export const HomePage = () => {
             </div>
             <div>
               <h3>Humidity</h3>
-              <p>1001 hpa</p>
+              <p>37 %</p>
             </div>
           </div>
           <div className="pressur_humidity">
