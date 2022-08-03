704ba6cf46da764c42739778fa8fcdce9fa41e4b
alokkumar-dev <alokshakya2601@gmail.com>
2022-08-03 11:57:49 +0530

    add functionality and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { HomePage } from "./Components/HomePage/HomePage";
 
 function App() {
-  // const getWeather = ()=>{
-  //   axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=18.5196&lon=73.8553&exclude=pune&appid=f54708523bef7b62ad34716409be8714&units=metric").then((res)=>{
-  //     console.log(res)
-  //   })
-  // }
-  // useEffect(()=>{
-  //     getWeather();
-  //   },[]);
-    // console.log(data)
   return (
     <div className="App">
       <HomePage />
-      
     </div>
   );
 }
